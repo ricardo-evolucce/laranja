85a40b82c885329f32c012b5f3694c00e828e9e4
ricardo <ricardo.evolucce@gmail.com>
2022-05-09 14:30:12 -0300

    novas paginas

---
+++ includes/footer.php
@@ -0,0 +1,49 @@
+		<footer id="footer" class="footer-texts-more-lighten">
+			<div class="container">
+				<div class="row py-5 my-1">
+					<div class="col-md-5 col-lg-5 mb-5 mb-lg-0 offset-md-2">
+						<h5 class="text-4 text-color-light mb-3">Contato</h5>
+						<ul class="list list-unstyled">
+							<li class="pb-1 mb-2">
+								<span class="d-block font-weight-normal line-height-1 text-color-light">Endereço</span> 
+								Av. Getúlio Vargas, 1151 e 1157 - sala 1013 - Menino Deus, Porto Alegre
+							</li>
+							<li class="pb-1 mb-2">
+								<span class="d-block font-weight-normal line-height-1 text-color-light">Whatsapp</span>
+								<a href="tel:+1234567890">(51) 99.815.2735</a>
+							</li>
+			
+						</ul>
+						
+					</div>
+					<div class="col-md-5 col-lg-5 mb-5 mb-lg-0 ">
+						<h5 class="text-4 text-color-light mb-3">Links Úteis</h5>
+						<ul class="list list-unstyled mb-0">
+							<li class="mb-0"><a href="cursos.php">Cursos</a></li>
+							<li class="mb-0"><a href="support-account.php">Support Account Startups</a></li>
+							<li class="mb-0"><a href="pericia-e-consultoria-contabil-porto-alegre.php">Perícia Contábil</a></li>
+							<li class="mb-0"><a href="pagamento-online.php">Pagamento Online</a></li>
+					
+						</ul>
+
+						<ul class="social-icons social-icons-clean-with-border social-icons-medium pt-3">
+						<li class="social-icons-facebook"><a href="https://www.instagram.com/laranjamarranghello/" target="_blank" title="Facebook"><i class="fab fa-instagram text-2"></i></a></li>
+
+									<li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in text-2"></i></a></li>
+							
+							
+						</ul>
+					</div>
+
+				</div>
+			</div>
+			<div class="container">
+				<div class="footer-copyright footer-copyright-style-2 pt-4 pb-5">
+					<div class="row">
+						<div class="col-12 text-center">
+							<p><strong>Laranja & Marranghello</strong> - © Copyright 2022. All Rights Reserved.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
